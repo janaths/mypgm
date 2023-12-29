22b0c97d08d0c3608ffa900867a68537d1268ca0
janaths <155150841+janaths@users.noreply.github.com>
2023-12-29 15:51:37 +0530

    This is my first file

---
+++ janath1.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    result = x+y
+    return result
+
+def sub(x, y):
+    return x-y
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=add(a,b)
+d=a+20
+e = sub(c, d)
+print(add(e, c))
+# print(a,"+",b,"=",result)
